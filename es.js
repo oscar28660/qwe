324b204a4636ef2a510f47e8476026919bae7a82
oscar28660 <94990918+oscar28660@users.noreply.github.com>
2021-11-24 14:19:55 -0400

    Add files via upload

---
+++ es.js
@@ -0,0 +1,2 @@
+var objTransJs = new Array();
+objTransJs["abrir_popup-alert"] = "Se esta intentando mostrar contenido por medio de una\nventana emergente y su navegador no lo permite.\nPor favor deshabilite su bloqueador de Pop-Pup";
